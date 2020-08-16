107d7d54c15591b2d972f711e837f1c45ffe6692
lalebdi <loai.alebdi@gmail.com>
2020-08-16 12:07:03 -0400

    Basic layout ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Chat from './components/Chat';
+import Join from './components/Join';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <Router>
+        <Route exact path="/" component={Join} />
+        <Route  path="/chat" component={Chat} />
+    </Router>
+)
 
-export default App;
+
+export default App
---
+++ src/components/Join.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Join() {
+    return (
+        <div>
+            Join
+        </div>
+    )
+}
+
+export default Join
