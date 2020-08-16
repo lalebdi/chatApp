9d03d9dba156662380d69361ad8117fbbc8c208e
lalebdi <loai.alebdi@gmail.com>
2020-08-16 12:48:05 -0400

    restuctured the files and did the styling

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Join.css';
 
 function Join() {
     const [name, setName] = useState('');
@@ -8,8 +9,8 @@ function Join() {
         <div className='joinOuterContainer'>
             <div className='joinInnerContainer'>
                 <h1 className="heading">Join</h1>
-                <div> <input className="joinInput" type="text" placeholder="Name" onChange{(event) => setName(event.target.value)} /> </div>
-                <div> <input className="joinInput mt-20" type="text" placeholder="Room" onChange{(event) => setRoom(event.target.value)} /> </div>
+                <div> <input className="joinInput" type="text" placeholder="Name" onChange{ (event) => setName(event.target.value)} /> </div>
+                <div> <input className="joinInput mt-20" type="text" placeholder="Room" onChange{ (event) => setRoom(event.target.value)} /> </div>
                 <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
                     <button className="button mt-20" type="submit" > Sign In </button>
                 </Link>
