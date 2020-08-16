f58c35991ea839b7ee51a8faa88cf323ecf95f0f
lalebdi <loai.alebdi@gmail.com>
2020-08-16 12:43:31 -0400

    done with the join layout

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Join() {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
     return (
-        <div>
-            Join
+        <div className='joinOuterContainer'>
+            <div className='joinInnerContainer'>
+                <h1 className="heading">Join</h1>
+                <div> <input className="joinInput" type="text" placeholder="Name" onChange{(event) => setName(event.target.value)} /> </div>
+                <div> <input className="joinInput mt-20" type="text" placeholder="Room" onChange{(event) => setRoom(event.target.value)} /> </div>
+                <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                    <button className="button mt-20" type="submit" > Sign In </button>
+                </Link>
+            </div>
         </div>
     )
 }
