3843a1e6228b895337be22aae734d9ab13dc6a48
lalebdi <loai.alebdi@gmail.com>
2020-09-10 20:24:09 -0400

    changed the server location for deployment

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -15,7 +15,7 @@ const Chat = ({ location }) => {
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://leah-whats-chat.herokuapp.com/';
     // location is from react-router we get a URL back
     useEffect(() => {
         // I destructured the data
